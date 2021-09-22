02207d57497f4485e4bd409ca8207f27b1d02d8b
Rohit Kapare <rakapare86@gmail.com>
2021-09-22 16:03:01 +0530

    Encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +14,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+const secret = "Thisisourlittlesecret."
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] }); // Add plugin to schema before creating its model.
 
 const User = new mongoose.model("User", userSchema);
 
@@ -58,7 +62,7 @@ app.post('/login', (req, res) => {
             if(foundUser.password === password) {
                 res.render("secrets");
             } else {
-                console.log("Password does not match.")
+                res.send("Password does not match.");
             }
         }
     });
