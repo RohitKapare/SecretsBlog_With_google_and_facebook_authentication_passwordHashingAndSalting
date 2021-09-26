38bc7c5fcfe24ce563ee9f5bcf475864c507b153
Rohit Kapare <rakapare86@gmail.com>
2021-09-26 14:13:24 +0530

    Google and FB OAuth and other fixes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -35,11 +38,22 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    username: String, // To resolve error caused by findOrCreate module.
+    password: String,
+    googleId: {
+        type: String,        
+        unique:true        
+    },
+    facebookId: {
+        type: String,
+        unique: true
+    },
+    secret: [String]
 });
 
 // For using passport-local-mongoose, Add it to mongoose schema as a plugin.
-userSchema.plugin(passportLocalMongoose) // This plugin is used to hash and salt a password.
+userSchema.plugin(passportLocalMongoose);// This plugin is used to hash and salt a password.
+userSchema.plugin(findOrCreate);
 
 // requires the model with Passport-Local Mongoose plugged in
 const User = new mongoose.model("User", userSchema);
@@ -50,14 +64,61 @@ passport.use(User.createStrategy());
 
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// This version of serialization code works for any stratigy e.g. local, google, etc.
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 
+passport.use(new GoogleStrategy({
+    clientID:     process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    // console.log(profile);
+    User.findOrCreate({googleId: profile.id,  }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/', (req, res) => {
     res.render("home");
+});
+
+app.get('/auth/google', passport.authenticate('google', { scope: [ "profile" ] }));
+
+app.get( '/auth/google/secrets', passport.authenticate( 'google', {
+        successRedirect: '/secrets',
+        failureRedirect: '/login'
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets', passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
 });
 
 app.get('/login', (req, res) => {
@@ -69,26 +130,57 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/secrets', (req, res) => {
+    let innerHTML = "Loading";
+    let href = "/secrets";
 
-    // The below line was added so we can't display the "/secrets" page
-    // after we logged out using the "back" button of the browser, which
-    // would normally display the browser cache and thus expose the 
-    // "/secrets" page we want to protect. 
-    res.set(
-        'Cache-Control', 
-        'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0'
-    );
-
+    // Fixed: Log Out button even if user is not logged in.
     if(req.isAuthenticated()){
-        res.render("secrets");
+        innerHTML = "Log Out";
+        href = "/logout";
     } else {
-        res.redirect("/login");
+        innerHTML = "Log In";
+        href = "/login";
     };
+
+    User.find({'secret': {$ne: null}}, (err, foundUser) => {
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser) {
+                res.render('secrets', {usersWithSecrets: foundUser, logOutBtnText: innerHTML, logOutBtnLink: href});
+            }
+        };
+    });
 });
 
 app.get('/logout', (req, res) => {
     req.logout();
     res.redirect("/");
+});
+
+app.get('/submit', (req, res) => {
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    };
+});
+
+app.post('/submit', (req, res) => {
+    const submittedSecret = req.body.secret;
+
+    // console.log(req.user) //It is user object saved by passport in http 'req' object.
+
+    User.findById(req.user.id, (err, foundUser) => {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                foundUser.secret.push(submittedSecret);
+                foundUser.save(() => {res.redirect('/secrets');});
+            }
+        }
+    });
 });
 
 app.post('/register', (req, res) => {
@@ -107,16 +199,6 @@ app.post('/register', (req, res) => {
 app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }),  function(req, res) {
 	res.redirect('/secrets');
 });
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, function () { 
     console.log("Server is started on port 3000");
